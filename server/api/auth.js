44a791d8c75d6cba1af608ad289d9a1c1faa7687
fauzi45 <uzi.fajar45@gmail.com>
2024-02-02 13:21:13 +0700

    feat: login employee

--- server/api/auth.js
+++ server/api/auth.js
@@ -4,14 +4,18 @@ const AuthHelper = require("../helpers/AuthHelper");
 
 const register = async (req, res) => {
   try {
-    const {name, email, password, position, departmentId} = req.body;
-    const response = await AuthHelper.registerEmployee({name, email, password, position, departmentId});
-    return res
-      .status(200)
-      .send({
-        message: "Data Employee succesfully registered",
-        data: response,
-      });
+    const { name, email, password, position, departmentId } = req.body;
+    const response = await AuthHelper.registerEmployee({
+      name,
+      email,
+      password,
+      position,
+      departmentId,
+    });
+    return res.status(200).send({
+      message: "Data Employee succesfully registered",
+      data: response,
+    });
   } catch (err) {
     res.status(400).send({
       message: "Data Employee failed registered",
@@ -20,6 +24,23 @@ const register = async (req, res) => {
   }
 };
 
-Router.post('/register', register);
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const response = await AuthHelper.login({ email, password });
+    return res.status(200).send({
+      message: "Login successfully",
+      data: response,
+    });
+  } catch (err) {
+    res.status(400).send({
+      message: "Login failed",
+      data: err.message,
+    });
+  }
+};
+
+Router.post("/register", register);
+Router.post("/login", login);
 
 module.exports = Router;
--- server/helpers/AuthHelper.js
+++ server/helpers/AuthHelper.js
@@ -1,44 +1,81 @@
-const Boom = require('boom');
-const bcrypt = require('bcryptjs');
-const _ = require('lodash');
-const jwt = require('jsonwebtoken');
+const Boom = require("boom");
+const bcrypt = require("bcryptjs");
+const _ = require("lodash");
+const jwt = require("jsonwebtoken");
 
-const db = require('../../models');
+const db = require("../../models");
 
-const jwtSecretToken = 'super_strong_key';
-const jwtExpiresIn = '24h';
+const jwtSecretToken = "super_strong_key";
+const jwtExpiresIn = "24h";
 const salt = bcrypt.genSaltSync(10);
 
 const __hashPassword = (password) => {
-    return bcrypt.hashSync(password, salt);
-}
+  return bcrypt.hashSync(password, salt);
+};
 
 const __comparePassword = (payloadPass, dbPass) => {
-    return jwt.sign(data, jwtSecretToken, {expiresIn: jwtExpiresIn});
-}
+  return bcrypt.compareSync(payloadPass, dbPass);
+};
+
+const __generateToken = (data) => {
+  return jwt.sign(data, jwtSecretToken, { expiresIn: jwtExpiresIn });
+};
 
 const registerEmployee = async (dataObject) => {
-    const { name, email, password, position, departmentId} = dataObject;
+  const { name, email, password, position, departmentId } = dataObject;
 
-    try {
-        const employee = await db.employees.findOne({
-            where: {email}
-        });
-        if (!_.isEmpty(employee)) {
-            return Promise.reject(Boom.badRequest('EMAIL HAS BEEN USED, PLEASE TRY ANOTHER EMAIL'));
-        }
-
-        const hashedPass = __hashPassword(password);
-        await db.employees.create({
-            name, email, password: hashedPass, position, departmentId
-        });
-
-        return Promise.resolve(true);
-    } catch (err) {
-        return Promise.reject(err);
+  try {
+    const employee = await db.employees.findOne({
+      where: { email },
+    });
+    if (!_.isEmpty(employee)) {
+      return Promise.reject(
+        Boom.badRequest("EMAIL HAS BEEN USED, PLEASE TRY ANOTHER EMAIL")
+      );
     }
-}
 
-module.exports= {
-    registerEmployee
-}
+    const hashedPass = __hashPassword(password);
+    await db.employees.create({
+      name,
+      email,
+      password: hashedPass,
+      position,
+      departmentId,
+    });
+
+    return Promise.resolve(true);
+  } catch (err) {
+    return Promise.reject(err);
+  }
+};
+
+const login = async (dataObject) => {
+  const { email, password } = dataObject;
+  try {
+    const employee = await db.employees.findOne({
+      where: { email },
+    });
+    if (_.isEmpty(employee)) {
+      throw new Error("Email tidak ada");
+    }
+
+    const isPassMatched = __comparePassword(password, employee.password);
+
+    if (!isPassMatched) {
+      return Promise.reject(Boom.badRequest("Wrong Password"));
+    }
+
+    const token = __generateToken({
+      name: employee.name,
+      password: employee.password,
+    });
+    return Promise.resolve({ token });
+  } catch (err) {
+    return Promise.reject(err);
+  }
+};
+
+module.exports = {
+  registerEmployee,
+  login,
+};
